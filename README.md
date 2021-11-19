2f7512c474369f85b5847bde7f6609820a851458
Dan Hercik <danhercik911@gmail.com>
2021-11-19 12:47:57 -0500

    added user-input for pixelize step, updated readme to reflect current functionality

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Re-implements the GIMP "Predator" image transformation in python using the cv li
 # algorithm details
 This image transformation follows these three steps:
 
-1 - "pixelize" (average pixels in an X by X area, the size of which is defined by the user) (currently unimplemented)
+1 - "pixelize" (average pixels in an X by X area, the size of which is defined by the user)
 
 2 - "min/max RGB" (finds the smallest or largest of the R, G, B values and set the others to zero)
 
@@ -14,15 +14,17 @@ This image transformation follows these three steps:
 At present, this program must be run in the terminal window.
 Make sure to chmod the file if you want to run it without the python command!
 
-command for changed mode: \[user@domain]$ ./predator.py image-path \[k-value] \[minmax]
+command for changed mode: \[user@domain]$ ./predator.py image-path \[k-value] \[minmax] \[pixelize]
 
-command for unchanged mode:\[user@domain]$ python predator.py image-path \[k-value] \[minmax]
+command for unchanged mode:\[user@domain]$ python predator.py image-path \[k-value] \[minmax] \[pixelize-size]
 
 image-path - path to a desired image. This is the only required argument.
 
 k-value - the k-value needed by the sobel transformation. A higher value results in thicker edges. - default of 3
 
 minmax - "min" or "max", chooses whether to select the minimum or maximum RGB value for each pixel - default of "min"
+
+pixelize - the X of the pixelize step's X by X area
 
 # image details
 Included in this repository is an example image to put through the transformation, as well as the output of the image when the default values are used.
--- predator.py
+++ predator.py
@@ -13,20 +13,20 @@ def minmax_rgb(i, w, h, m):
                 minmax = min(i[x][y][0],i[x][y][1],i[x][y][2])
             else:
                 minmax = max(i[x][y][0],i[x][y][1],i[x][y][2])
-            if minmax == i[x][y][0]: # blue is least
+            if minmax == i[x][y][0]: # blue is least/most
                 i[x][y][1] = 0
                 i[x][y][2] = 0 
-            if minmax == i[x][y][1]: # green is least
+            if minmax == i[x][y][1]: # green is least/most
                 i[x][y][0] = 0
                 i[x][y][2] = 0
-            if minmax == i[x][y][2]: # red is least       
+            if minmax == i[x][y][2]: # red is least/most
                 i[x][y][0] = 0
                 i[x][y][1] = 0
             y+=1
         x+=1 
     return i
     
-def pixelize(i, pix_w, pix_h):
+def pixelize(i, s, pix_h):
     i_h, i_w = i.shape[:2]
     # shrink image
     i = cv.resize(i, (pix_w, pix_h), interpolation=cv.INTER_CUBIC)
@@ -35,22 +35,33 @@ def pixelize(i, pix_w, pix_h):
     return pixelized
 
 def sobel(i, k):
-    # the general convention is to use a gaussian blur on the image, 
+    # the general convention is to use a gaussian blur on the image,
     # but the pixelization should take care of that here.
     sobx = cv.Sobel(i,cv.CV_16S,1,0,ksize=k,scale=1,delta=0)
     soby = cv.Sobel(i,cv.CV_16S,0,1,ksize=k,scale=1,delta=0)
     abSobx = cv.convertScaleAbs(sobx) # makes the sobels absolute
     abSoby = cv.convertScaleAbs(soby)
-    # approximates the sobel algo
-    sobtot = cv.addWeighted(abSobx, 0.5, abSoby, 0.5, 0)  
-    cv.imshow('sobel\'d', sobtot)
+    # approximates the sobel algo using built-in cv2 functions
+    # the more precise methods produced far less consistent output,
+    # likely due to floating-point rounding errors
+    sobtot = cv.addWeighted(abSobx, 0.5, abSoby, 0.5, 0)
+    cv.imshow('output', sobtot)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
-pix_w, pix_h = (64, 64)
-k = 3
-mode = 'min'
-if len(sys.argv) > 3:
+# check if a valid filename is given
+try:
+   i = cv.imread(sys.argv[1])
+   hig, wid, _ = i.shape
+except:
+    print("Error: image does not exist")
+    sys.exit(1)
+
+k = 3 # the 'k' value for the sobel filter. default of 3
+mode = 'min' # can choose to minimize or maximize RGB. default of min
+shrinkage = 3 # amount of pixelization averaging. default of 3x3 area
+
+if len(sys.argv) > 2: # if we have a second arg, it should be k
     k = int(sys.argv[2])
     if k%2 == 0:
         print("Error: k must be odd.")
@@ -58,18 +69,15 @@ if len(sys.argv) > 3:
     if k > 31:
         print("Error: k must be less than 32")
         sys.exit(1)
+if len(sys.argv) > 3: # third argument should be min or max
     mode = sys.argv[3]
-    if mode != 'min' or mode != 'max':
+    if mode != 'min' and mode != 'max':
         print("Error: third flag must be \"min\" or \"max\"")
         sys.exit(1)
-    pix_w = int(sys.argv[4])
-    pix_h = int(sys.argv[5])
-try:
-   i = cv.imread(sys.argv[1])
-   hig, wid, _ = i.shape
-except:
-    print("Error: image does not exist")
-    sys.exit(1)
+if len(sys.argv) > 4: # last arg should be the amount of pixelization
+    shrinkage = int(sys.argv[4])
+
+pix_w, pix_h = (int(wid/shrinkage), int(hig/shrinkage))
 j = minmax_rgb(i, wid, hig, mode)
 i = pixelize(j, pix_w, pix_h)
 sobel(i, k)
